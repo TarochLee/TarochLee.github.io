c20f813886995b8095d92f92029483cf6805acd1
TarochLee <1334996241@qq.com>
2025-02-01 10:55:11 +0800

    测试

--- games/pacman/static/script/index.js
+++ games/pacman/static/script/index.js
@@ -579,7 +579,7 @@
 			x:game.width-10,
 			y:game.height-5,
 			draw:function(context){
-				var text = '  ';
+				var text = '© passer-by.com';
 				context.font = '12px/20px PressStart2P';
 				context.textAlign = 'left';
 				context.textBaseline = 'top';
